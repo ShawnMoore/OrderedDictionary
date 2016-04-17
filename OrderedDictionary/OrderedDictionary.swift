2d10df534ed2a86207df2fa964a34bb0aa068847
ShawnMoore <sm5@me.com>
2016-04-17 12:09:32 -0500

    All functionality of Swift's Dictionary

--- OrderedDictionary/OrderedDictionary.swift
+++ OrderedDictionary/OrderedDictionary.swift
@@ -16,6 +16,12 @@ public struct OrderedDictionary<Key: Hashable, Value>: CollectionType, Dictionar
     public typealias Element = (Key, Value)
     
     // MARK: - Instance Properties
+    private(set) var keys: [Key]
+    
+    public var values: [Value] {
+        return map({$0.1})
+    }
+    
     public var startIndex: Index {
         return _dictionary.startIndex
     }
@@ -33,12 +39,11 @@ public struct OrderedDictionary<Key: Hashable, Value>: CollectionType, Dictionar
     }
     
     // MARK: - Private Instance Properties
-    private var _keys: [Key]
     private var _dictionary: Dictionary<Key, Value>
     
     // MARK: - Initializers
     public init() {
-        _keys = []
+        keys = []
         _dictionary = [:]
     }
     
@@ -46,14 +51,14 @@ public struct OrderedDictionary<Key: Hashable, Value>: CollectionType, Dictionar
         self.init()
         
         for element in elements {
-            _keys.append(element.0)
+            keys.append(element.0)
             _dictionary[element.0] = element.1
         }
     }
     
     public init(minimumCapacity: Int) {
-        _keys = []
-        _keys.reserveCapacity(minimumCapacity)
+        keys = []
+        keys.reserveCapacity(minimumCapacity)
         _dictionary = Dictionary(minimumCapacity: minimumCapacity)
     }
     
@@ -62,8 +67,8 @@ public struct OrderedDictionary<Key: Hashable, Value>: CollectionType, Dictionar
         var index = 0
         
         return AnyGenerator<Element> {
-            if let value = self._dictionary[self._keys[index]] where index < self._keys.count {
-                let returnValue = (self._keys[index], value)
+            if let value = self._dictionary[self.keys[index]] where index < self.keys.count {
+                let returnValue = (self.keys[index], value)
                 index += 1
                 return returnValue
             }
@@ -71,8 +76,79 @@ public struct OrderedDictionary<Key: Hashable, Value>: CollectionType, Dictionar
         }
     }
     
+    @warn_unused_result func indexForKey(key: Key) -> Index? {
+        return _dictionary.indexForKey(key)
+    }
+    
+    mutating func popFirst() -> Element? {
+        guard !self.isEmpty else { return nil }
+        
+        let key = keys.removeFirst()
+        
+        if let value = _dictionary[key] {
+            return (key, value)
+        } else {
+            keys.insert(key, atIndex: 0)
+        }
+        
+        return nil
+    }
+    
+    mutating func removeAll(keepCapacity keepCapacity: Bool = false) {
+        _dictionary.removeAll(keepCapacity: keepCapacity)
+        keys.removeAll(keepCapacity: keepCapacity)
+    }
+    
+    mutating func removeAtIndex(index: DictionaryIndex<Key, Value>) -> (Key, Value) {
+        let element = _dictionary.removeAtIndex(index)
+        
+        if let index = keys.indexOf(element.0) {
+            keys.removeAtIndex(index)
+        }
+        
+        return element
+    }
+    
+    mutating func removeValueForKey(key: Key) -> Value? {
+        guard let value = _dictionary.removeValueForKey(key) else { return nil }
+        
+        if let index = keys.indexOf(key) {
+            keys.removeAtIndex(index)
+        }
+        
+        return value
+    }
+    
+    mutating func updateValue(value: Value, forKey key: Key) -> Value? {
+        let value = _dictionary.updateValue(value, forKey: key)
+        
+        if value == nil {
+            _dictionary[key] = value
+            keys.append(key)
+        }
+        
+        return value
+    }
+    
     // MARK: - Subscripts
     public subscript(index: Index) -> Element {
         return _dictionary[index]
+    }
+    
+    subscript (key: Key) -> Value? {
+        get {
+            return _dictionary[key]
+        }
+        set {
+            if let value = newValue {
+                _dictionary[key] = value
+                
+                if keys.indexOf(key) == nil {
+                    keys.append(key)
+                }
+            } else {
+                removeValueForKey(key)
+            }
+        }
     }
 }
